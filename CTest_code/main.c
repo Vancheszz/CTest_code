077383c559272976fa05f2468852da52b6280417
vancheszz <vancheszzgh@gmail.com>
2025-09-17 19:27:52 +0300

    Initial Commit

---
+++ CTest_code/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CTest_code
+//
+//  Created by Shtrikh on 17.09.2025.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
