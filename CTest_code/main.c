1b7398c94a35963879fdf9ca48cd6354e59bda84
vancheszz <vancheszzgh@gmail.com>
2025-09-18 23:24:23 +0300

    del

--- CTest_code/main.c
+++ CTest_code/main.c
@@ -3,10 +3,16 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include "pid_test.h"
+//#include "pid_test.h"
+#include "exec_files.h"
 int main(int argc, const char * argv[]) {
     //test pid
     //testing_pid();
-    pipe_fork();
+    //pipe_fork();
+    if (argc != 2 ){
+        printf("usage: app path");
+    }else{
+        exec_test_1(argv[1]);
+    }
     return EXIT_SUCCESS;
 }
