407d65ab0545ad128d304ff94df62a7e4e703506
vancheszz <vancheszzgh@gmail.com>
2025-09-19 02:22:31 +0300

    NEW FILES

---
+++ CTest_code/Signals/signal.c
@@ -0,0 +1,8 @@
+//
+//  signal.c
+//  CTest_code
+//
+//  Created by Shtrikh on 19.09.2025.
+//
+
+#include "signal.h"
---
+++ CTest_code/Signals/signal.h
@@ -0,0 +1,13 @@
+//
+//  signal.h
+//  CTest_code
+//
+//  Created by Shtrikh on 19.09.2025.
+//
+
+#ifndef signal_h
+#define signal_h
+
+#include <stdio.h>
+
+#endif /* signal_h */
